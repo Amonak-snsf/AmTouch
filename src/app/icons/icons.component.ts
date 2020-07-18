9ddfd6cc9ecf911f6e9a456a5ebd18830c0c5f8d
amonak-maker <jeanpkokora@gmail.com>
2020-07-18 18:25:34 +0200

    jp update 5
    jp update 5

--- src/app/icons/icons.component.ts
+++ src/app/icons/icons.component.ts
@@ -12,5 +12,91 @@ export class IconsComponent implements OnInit {
   ngOnInit() {
   }
 
-
+  showIco1(){
+  document.getElementById("ico1").style.display = "block";
+  }
+  showIco2(){
+  document.getElementById("ico2").style.display = "block";
+  }
+  showIco3(){
+  document.getElementById("ico3").style.display = "block";
+  }
+  showIco4(){
+  document.getElementById("ico4").style.display = "block";
+  }
+  showIco5(){
+  document.getElementById("ico5").style.display = "block";
+  }
+  showIco6(){
+  document.getElementById("ico6").style.display = "block";
+  }
+  showIco7(){
+  document.getElementById("ico7").style.display = "block";
+  }
+  showIco8(){
+  document.getElementById("ico8").style.display = "block";
+  }
+  showIco9(){
+  document.getElementById("ico9").style.display = "block";
+  }
+  showIco10(){
+  document.getElementById("ico10").style.display = "block";
+  }
+  showIco11(){
+  document.getElementById("ico11").style.display = "block";
+  }
+  showIco12(){
+  document.getElementById("ico12").style.display = "block";
+  }
+  showIco13(){
+  document.getElementById("ico13").style.display = "block";
+  }
+  showIco14(){
+  document.getElementById("ico14").style.display = "block";
+  }
+  showIco15(){
+  document.getElementById("ico15").style.display = "block";
+  }
+  showIco16(){
+  document.getElementById("ico16").style.display = "block";
+  }
+  showIco17(){
+  document.getElementById("ico17").style.display = "block";
+  }
+  showIco18(){
+  document.getElementById("ico18").style.display = "block";
+  }
+  showIco19(){
+  document.getElementById("ico19").style.display = "block";
+  }
+  showIco20(){
+  document.getElementById("ico20").style.display = "block";
+  }
+  showIco21(){
+  document.getElementById("ico21").style.display = "block";
+  }
+  showIco22(){
+  document.getElementById("ico22").style.display = "block";
+  }
+  showIco23(){
+  document.getElementById("ico23").style.display = "block";
+  }
+  showIco24(){
+  document.getElementById("ico24").style.display = "block";
+  }
+  showIco25(){
+  document.getElementById("ico25").style.display = "block";
+  }
+  showIco26(){
+  document.getElementById("ico26").style.display = "block";
+  }
+  showIco27(){
+  document.getElementById("ico27").style.display = "block";
+  }
+  showIco28(){
+  document.getElementById("ico28").style.display = "block";
+  }
+  showIco29(){
+  document.getElementById("ico29").style.display = "block";
+  }
 }
